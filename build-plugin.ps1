162c967abd6202e70f7a6f773d5cad870e2fb400
Anthony Steiner <asteiner@steinerd.com>
2022-09-19 16:25:44 -0400

    Marketplace contribution tweaks to meta files and deployment script.

--- build-plugin.ps1
+++ build-plugin.ps1
@@ -9,10 +9,15 @@ $pluginName = "$outputFileName.lplug4"
 $loupedeckYaml = "LoupedeckPackage.yaml"
 $cwd = Get-Location
 
+if( Test-Path $buildPath){
+	Remove-Item -Path "$buildPath" -Force -Recurse 
+}
+
 New-Item -Path "$buildPath" -Force -Name "bin" -ItemType "directory" > $null
 New-Item -Path "$buildPath" -Force -Name "metadata" -ItemType "directory" > $null
 
 Copy-Item "$loupedeckYaml" -Force -Destination "$buildPath\metadata\$loupedeckYaml" > $null
+Copy-Item "$($project)Plugin\Resources\256.png" -Force -Destination "$buildPath\metadata\256.png" > $null
 Copy-Item "$dllPath\$dllName" -Force -Destination "$buildPath\bin\$dllName" > $null
 
 $compress = @{
